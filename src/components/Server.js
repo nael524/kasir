462726e36c488ac01e7126259f80eaf0d506cf41
nael <2403310164@students.satyaterrabhinneka.ac.id>
2025-12-19 07:54:58 +0700

    webkasir

--- src/components/Server.js
+++ src/components/Server.js
@@ -1,24 +1,13 @@
 import express from "express";
 import cors from "cors";
+import "dotenv/config";
 
 const app = express();
-
 app.use(cors());
 app.use(express.json());
 
-// ROUTE REGISTER
 app.post("/api/register", (req, res) => {
-  const { firstname, lastname, username, password } = req.body;
-
-  if (!firstname || !lastname || !username || !password) {
-    return res.status(400).json({
-      message: "Data tidak lengkap",
-    });
-  }
-
-  return res.status(201).json({
-    message: "Akun anda berhasil dibuat",
-  });
+  res.json({ message: "Akun anda berhasil dibuat" });
 });
 
 const PORT = process.env.PORT || 3000;
